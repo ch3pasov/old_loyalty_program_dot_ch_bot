9d568cdec962c95fa420dc37058b7cde1be143e8
Anatoliy Ch <50632903+ch3pasov@users.noreply.github.com>
2022-11-13 17:31:10 +0100

    помечаю как удалённые (какие-то траблы с игнором)

--- server/secret.py
+++
@@ -1,5 +0,0 @@
-# Для работы Pyrogram нужен Telegarm API key из https://my.telegram.org/apps
-# Подробнее https://docs.pyrogram.org/intro/quickstart
-
-api_id = 0  # App api_id
-api_hash = "a"  # App api_hash
