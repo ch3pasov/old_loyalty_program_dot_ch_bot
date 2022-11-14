0f6b3f6cd25f87dae2d471ee0effc482b66bb214
Anatoliy Ch <50632903+ch3pasov@users.noreply.github.com>
2022-11-14 15:48:55 +0100

    Создал server_default

---
+++ server_default/secret.py
@@ -0,0 +1,5 @@
+# Для работы Pyrogram нужен Telegarm API key из https://my.telegram.org/apps
+# Подробнее https://docs.pyrogram.org/intro/quickstart
+
+api_id = 0  # App api_id
+api_hash = "a"  # App api_hash
---
+++ server_default/server_vars.py
@@ -0,0 +1,21 @@
+money_chat_id = -0  # id чата, куда Человек будет кидать деньги
+dot_ch_id = -0  # id канала, на который нужно быть подписанным
+# dot_ch_chat_id = -1  # id чата канала
+
+# Ой вот тут вы умрёте доставать этот id, для каждого робота он разный. Удачи!
+money_animation = "a"
+
+# программа лояльности
+loyality_programm = [
+    {"level": 0, "days": 0, "reward": 0.0001},
+    {"level": 1, "days": 1, "reward": 0.0002},
+    {"level": 2, "days": 2, "reward": 0.0004},
+    {"level": 3, "days": 3, "reward": 0.0008},
+    {"level": 4, "days": 4, "reward": 0.0016},
+    {"level": 5, "days": 5, "reward": 0.0032},
+    {"level": 6, "days": 6, "reward": 0.0064},
+    {"level": 7, "days": 7, "reward": 0.01},
+    {"level": 8, "days": 14, "reward": 0.01},
+    {"level": 9, "days": 21, "reward": 0.01},
+    {"level": 10, "days": 28, "reward": 0.05},
+]
