ab546f0b3b87a8c5580c950fc2763f322632a2c1
Anatoliy Ch <50632903+ch3pasov@users.noreply.github.com>
2022-12-10 13:49:57 +0100

    пусть по умолчанию нет алёртов

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ print(f"Я запустил main и смотрю на users. Его id {id(users
 
 if __name__ == '__main__':
     try:
-        app.send_message(
-            chat_id=server.server_vars.dot_ch_chat_id,
-            text='Я запустился!',
-            reply_to_message_id=server.server_vars.bot_debug_message_id
-        )
+        # app.send_message(
+        #     chat_id=server.server_vars.dot_ch_chat_id,
+        #     text='Я запустился!',
+        #     reply_to_message_id=server.server_vars.bot_debug_message_id
+        # )
 
         schedule.start_scheduler(users, verbose=False)
         print(f"Я запустил name=main в main и смотрю на users. Его id {id(users)}")
         idle()
     finally:
         print('FINALLY')
-        app.send_message(
-            chat_id=server.server_vars.dot_ch_chat_id,
-            text='Я выключился! @yandex_links, обрати внимание, если это незапланированное выключение.',
-            reply_to_message_id=server.server_vars.bot_debug_message_id
-        )
+        # app.send_message(
+        #     chat_id=server.server_vars.dot_ch_chat_id,
+        #     text='Я выключился! @yandex_links, обрати внимание, если это незапланированное выключение.',
+        #     reply_to_message_id=server.server_vars.bot_debug_message_id
+        # )
         schedule.backup_log_job(users, verbose=True)
         schedule.save_log_job(users, verbose=True)
