2b70b502a08b1e45f634bf460c40cbdad71da889
Anatoliy Ch <50632903+ch3pasov@users.noreply.github.com>
2023-06-21 12:51:11 +0200

    Операция по спасению 👸

--- queue_program/queue_schedule.py
+++ queue_program/queue_schedule.py
@@ -337,6 +337,9 @@ def restore_queue_users():
 
         queue_id = queue_user["in"]["id"]
         if queue_user["in"]["type"] == "queue":
+            if queue_id not in active_queues:
+                print(f"⁉️ {user_id} in_queue problem (queue not exists). Fixed it!")
+                queue_user["in"] = None
             if user_id not in active_queues[queue_id]['queue_order']:
                 print(f"⁉️ {user_id} in_queue problem (not in queue, but non-empty). Fixed it!")
                 queue_user["in"] = None
