cb3a762171fab35c520bcaaa18588e6ec58f4954
Anatoliy Ch <50632903+ch3pasov@users.noreply.github.com>
2023-06-30 21:49:34 +0200

    Теперь манидроп появляется в случайное время через 1-5 минут

--- loyalty_program/moneydrop/moneydrop_handlers.py
+++ loyalty_program/moneydrop/moneydrop_handlers.py
@@ -2,6 +2,7 @@ from global_vars import app
 from pyrogram import filters
 from server.server_vars import dot_ch_id, dot_ch_chat_id, money_drop_amount
 from time import sleep
+from random import random
 from lib.money import money_drop
 
 last_media_group_id = 0
@@ -14,8 +15,9 @@ def start_post_moneydrop_handlers():
         if message.sender_chat.id == dot_ch_id and message.chat.id == dot_ch_chat_id:
             if message.media_group_id != last_media_group_id:
                 last_media_group_id = message.media_group_id if message.media_group_id else 0
-                print('New post! Sleep!')
-                sleep(15)
+                sleep_time = 60+240*random()
+                print(f'New post! Sleep {sleep_time}')
+                sleep(sleep_time)
                 print('Post-moneydrop!')
                 money_drop(
                     dot_ch_chat_id,
